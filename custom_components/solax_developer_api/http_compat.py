e430aa38a387734a672bd5e60a1d7a30bb19dd1a
NoUsername10 <71009757+NoUsername10@users.noreply.github.com>
2026-08-09 15:00:57 +0200

    Add files via upload

---
+++ custom_components/solax_developer_api/http_compat.py
@@ -0,0 +1,5 @@
+"""Home Assistant HTTP compatibility imports."""
+
+from homeassistant.components.http import StaticPathConfig
+
+__all__ = ["StaticPathConfig"]
---
+++ custom_components/solax_developer_api/http_compat.pyi
@@ -0,0 +1,5 @@
+"""Typed Home Assistant HTTP compatibility imports."""
+
+from homeassistant.components.http.server import StaticPathConfig as StaticPathConfig
+
+__all__ = ["StaticPathConfig"]
